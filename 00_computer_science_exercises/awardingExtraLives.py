49de4894023b7f48f5218fd65e5c91a5c9609b55
unknownkeyon <s11940533@students.duvalschools.org>
2024-09-10 13:20:35 -0400

    live
    life

---
+++ 00_computer_science_exercises/awardingExtraLives.py
@@ -0,0 +1,15 @@
+# Awarding Extra Lives, Keyon Walker, v0.0
+
+
+lives = 3
+
+# Allow the user to input the score.
+
+# If score is 10000 or less
+    # Lose a Life
+# If score is > 10000 but less than 100001
+    # Give 1 Extra Life
+# If score is > 100000
+    # Give 2 Extra Lives
+
+# Output the score and number of lives to the screen.
