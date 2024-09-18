bbdf9c95a777a2197396eee83974c4f9168feb10
unknownkeyon <s11940533@students.duvalschools.org>
2024-09-18 13:15:53 -0400

    Update rps.py
    rock paper

--- 01_gaming_exercises/rps.py
+++ 01_gaming_exercises/rps.py
@@ -20,5 +20,22 @@ isCorrect = input("Is that correct?  Type yes or no and press enter.\n")
 if isCorrect == "yes":
     print(f"Ok {playerName}, let's play Rock, Paper, Scissors!\n")
 else:
-    playerName = input
+    playerName = input("Please type your name and press enter.\n")
+
+# THE RULES using MULTI-LINE STRINGS
+print(""
+Welcome, {keyon} to the Rock, Paper, Scissors Robot!
+It's Time To Play Rock, Paper, Scissors!
+      
+You will play against the CPU.  The first player to score 5 points wins.
+You will select from ROCK, PAPER, OR SCISSORS.
+The CPU will select ROCK, PAPER, SCISSORS at random.
+
+# MAIN GAME LOOP
+while playerScore < 5 and cpuScore < 5:
+     print(f"{keyon} you have {3} points.\nThe CPU has {4} points.\n")
+     playerChoice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
+
+
+
 
