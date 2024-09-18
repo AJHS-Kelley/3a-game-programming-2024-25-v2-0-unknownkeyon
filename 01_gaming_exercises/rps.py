d01665a72c99c7985899c12c538e7cfbfc3a6f9b
unknownkeyon <s11940533@students.duvalschools.org>
2024-09-18 11:50:15 -0400

    rps
    player name

---
+++ 01_gaming_exercises/rps.py
@@ -0,0 +1,14 @@
+# Rock, Paper, Scissors by Keyon Walker, v0.0
+
+# MODULE IMPORTS
+import random
+
+# DATA STRUCTURES
+playerScore = 0
+cpuScore = 0
+playerChoice = None
+
+# DATA STRUCTURES -- CPU
+playerName = "Test Player"
+playerChoice = None
+cpuChoice = None
