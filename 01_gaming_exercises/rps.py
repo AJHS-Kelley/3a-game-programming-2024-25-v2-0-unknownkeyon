77a4c604de7f108997eb6119a79c5f157ec062c5
unknownkeyon <s11940533@students.duvalschools.org>
2024-09-18 12:04:31 -0400

    Update rps.py

--- 01_gaming_exercises/rps.py
+++ 01_gaming_exercises/rps.py
@@ -9,6 +9,16 @@ cpuScore = 0
 playerChoice = None
 
 # DATA STRUCTURES -- CPU
-playerName = "Test Player"
-playerChoice = None
+cpuScore = 0
 cpuChoice = None
+
+# PLAYER NAME INPUT
+playerName = input("Please type your name and press enter.\n")
+print(f"Hello {playerName}!\n")
+isCorrect = input("Is that correct?  Type yes or no and press enter.\n")
+
+if isCorrect == "yes":
+    print(f"Ok {playerName}, let's play Rock, Paper, Scissors!\n")
+else:
+    playerName = input
+
