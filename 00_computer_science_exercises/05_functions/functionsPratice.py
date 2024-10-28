fc4aaf1964910c480d73db64235067f92e4660b4
unknownkeyon <s11940533@students.duvalschools.org>
2024-10-28 12:05:09 -0400

    languages
    heloow

---
+++ 00_computer_science_exercises/05_functions/functionsPratice.py
@@ -0,0 +1,12 @@
+# Functions Practice, Ryan Kelley, v0.0
+
+import random
+
+def helloWorldMulti(): # FUNCTION SIGNATURE
+    """This function will output Hello, Worlf! in a language the user choose. """ # DOCSTRING \
+    # print a list of languages to the screen, at least three but no more than five.
+    # allow the user to select (input) a choice for the language.
+    # print "Hello, World!" to the screen in that language.
+
+    print("""english", "france""")
+    language = input("What language do you want?\n  Please type the first letter of the language you want.\n").lower()
